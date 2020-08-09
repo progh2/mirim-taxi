063c9d20cba9f9ba135337354cc84226f3ab7b80
Gihun Ham <ham@e-mirim.hs.kr>
2020-08-09 14:22:15 +0900

    #2 - BottomNavigationBar 위젯을 이용한 하단 탭 구성하기. 간단하고 예쁘다

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _index = 0; // 페이지 인덱스
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('복잡한 UI'),
+      ),
+      body:Center(
+        child: Text(
+          '$_index 페이지',
+          style: TextStyle( fontSize: 40),
+        )
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index){
+          setState(() {
+            _index = index;
+          });
+        },
+        currentIndex: _index,
+        items:<BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title:Text('홈'),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title:Text('이용서비스'),
+            icon: Icon(Icons.assignment),
+          ),
+          BottomNavigationBarItem(
+            title:Text('내 정보'),
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+      ),
+
+    );
   }
 }
